663def5db567d82dcc1239ce78776f89b32ee03a
gustavokubiack <guukubiack@gmail.com>
2025-03-31 08:38:35 -0300

    feat: registrar model task no admin

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from tasks.models import Task
 
-# Register your models here.
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'status',)
+    search_fields = ('title', 'status',)
+    list_filter = ('status',)
